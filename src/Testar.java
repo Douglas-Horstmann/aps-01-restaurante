a96afc80f5ab8ed60ee842e76ed8c6e6bcf96185
douglas-horstmann <douglas.ahorstmann@gmail.com>
2017-09-03 23:36:00 -0300

    Primeiro commit do projeto

---
+++ src/Testar.java
@@ -0,0 +1,13 @@
+
+public class Testar {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Prato p = new Prato("Prato 1", 50.00);
+		System.out.println("Nome: " + p.GetNome() + " -- Preço: " + p.GetPreco());
+		
+
+	}
+
+}
