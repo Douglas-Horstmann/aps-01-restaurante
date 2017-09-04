f95af01d7bb72f7641601ec58b78693c6fac9e18
douglas-horstmann <douglas.ahorstmann@gmail.com>
2017-09-03 23:39:26 -0300

    Inclusão da classe Prato

---
+++ src/Prato.java
@@ -0,0 +1,43 @@
+
+public class Prato {
+	
+	private String nome;
+	private double preco;
+	private String descricao;
+	
+	public Prato(String nome, double preco){
+		this.SetNome(nome);
+		this.SetPreco(preco);
+	}
+	
+	public Prato(String nome, double preco, String descricao){
+		this.SetNome(nome);
+		this.SetPreco(preco);
+		this.SetDescricao(descricao);
+	}
+	
+	private void SetNome(String nome){
+		this.nome = nome;
+	}
+	
+	public void SetPreco(double preco){
+		this.preco = preco;
+	}
+	
+	public void SetDescricao(String descricao){
+		this.descricao = descricao;
+	}
+	
+	public String GetNome(){
+		return this.nome;
+	}
+	
+	public double GetPreco(){
+		return this.preco;
+	}
+	
+	public String GetDescricao(){
+		return this.descricao;
+	}
+
+}
